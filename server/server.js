e0ec84a5e98a24468dc0a3ba19e4ac1cd8e38ee9
PabloRibeiro-Br <75342853+PabloRibeiro-Br@users.noreply.github.com>
2023-11-21 05:26:30 -0300

    update2
    update da especializacao do codigo

--- server/server.js
+++ server/server.js
@@ -23,16 +23,33 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const topic = req.body.topic; // Assuming you send the topic as part of the request
+
+    let prompt;
+
+    // Customize the prompt based on the specified topic
+    switch (topic) {
+      case 'academia':
+        prompt = 'Discuss the latest trends and advancements in academia.';
+        break;
+      case 'fisiculturismo':
+        prompt = 'Provide information on effective fisiculturismo training techniques.';
+        break;
+      case 'saude':
+        prompt = 'Discuss the importance of maintaining a healthy lifestyle for overall well-being.';
+        break;
+      default:
+        prompt = 'Default prompt for general queries.';
+    }
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 200, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      temperature: 0,
+      max_tokens: 200,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
     });
 
     res.status(200).send({
